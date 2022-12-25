768e04852a01512ab5ea9dfcc28d15787fc75044
AlekseiPanov <panov.a.a60@gmail.com>
2022-12-25 13:22:52 +0300

    sem4

---
+++ sem4/example27.py
@@ -0,0 +1,24 @@
+# Пользователь вводит текст(строка). Словом считается последовательность непробельных символов
+# идущих подряд, слова разделены одним или большим числом пробелов или символами конца строки.
+# Определите, сколько различных слов содержится в этом тексте.
+# Input: She sells sea shells on the sea shore;The shells that she sells are sea shells
+# I'm sure.So if she sells sea shells on the sea shore,I'm sure that the shells are sea
+# shore shells.
+
+# Output: 19
+
+st = "She sells sea shells on the sea shore;\
+    The shells that she sells are sea shells I'm sure.\
+    So if she sells sea shells on the sea shore,\
+    I'm sure that the shells are sea shore shells."
+print(st)
+st = st.lower()
+st = st.replace(',', " ")
+st = st.replace(';', " ")
+st = st.replace('.', " ")
+st = st.replace('\'', "")
+
+
+words = set(st.split())
+print(words)
+print(len(words))
