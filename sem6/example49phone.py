87d7acd13478e9d78fa05bf641cb491808933652
AlekseiPanov <panov.a.a60@gmail.com>
2023-01-23 18:27:14 +0300

    hw2

--- sem6/example49phone.py
+++ sem6/example49phone.py
@@ -8,57 +8,84 @@
 
 fileName = 'tel.txt'
 
-def writeFile(file_name):
+def writeFile(file_name):               
+    '''Добавление новой записи'''
     with open(fileName, 'a') as data:
-        for item in range(4):
-            item = input('>: ')
-            data.writelines(item + ',')
+        print('Введите ФИО и телефон именно в таком порядке')
+        data.writelines('\n')
+        item = ''
+        for _ in range(4):
+            item += input('>: ')
+            item += ','
+        data.writelines(item[:-1])
 
-def save_data_to_file(data_to_save):
-    '''запись строки данных в конец файла'''
-    data_to_save = ",".join(data_to_save) + "\n"
-    print(data_to_save)
-    with open(fileName, "a", encoding="utf8") as datafile:
-        datafile.write(data_to_save)
-
-def add_data():
-    '''добавление записи'''
-    while True:
-        print('Добавление записи(""-выход)>:')
-        last_name = input("Фамилия: ")
-        if last_name == "":
-            return
-        first_name = input("Имя: ")
-        patronymic = input("Отчество: ")
-        phone_number = input("Номер Телефона: ")
-        data_to_save = [last_name, first_name, patronymic, phone_number]
-        if "" in data_to_save:
-            return
-        save_data_to_file(data_to_save)
-
-
-def readFile(file_name):
+def readFile(file_name): 
     result = []
     with open(fileName, 'r+') as data:
         for line in data:
             result.append(line.split(','))
         return result
 
-def findUser(userList, fragment):
-    for user in userList:
-        if user[1] == fragment:
-            print(user[0],user[1],user[2],'  -  ',user[3])
+def findUser():
+    fragment = input('Введите поисковый запрос: ')      #Нийти запись по части имени или телефона. надо разобраться с переносами строк
+    with open(fileName, 'r') as userList:
+        print(userList)
+        for user in userList:
+            if fragment in user:
+                print(user[0],user[1],user[2],'  -  ',user[3])
 
-def showAllUser(printdata):
+def showAllUser():
     with open(fileName, 'r') as datafile:
         for line in datafile:
             newline = line.strip('\n').split(',')
             print('{} {} {}  -  {}'.format(newline[0], newline[1], newline[2], newline[3]))
 
 
+def deletUser():
+    print()
+
+def changePhoneNumber():
+    print()
 
 # print(readFile(fileName))
 # fragment = input()
 # findUser(readFile(fileName), fragment)
 # showAllUser(fileName)
-writeFile(fileName)
+# writeFile(fileName)
+
+if __name__ == "__main__":
+    # основной блок
+    menu = (f"Введите команду\n"
+            "1 - Показать все записи\n"
+            "2 - Найти запись по части имени или телефону\n"
+            "3 - Добавить новый контакт\n"
+            '4 - Удалить контакт\n'
+            '5 - Изменить номер телефона у контакта\n'
+            "6 - Выход\n")
+    while True:
+        print(menu)
+        answer = input(">:")
+        match answer:
+            case "1":
+                # Показать все записи
+                showAllUser()
+
+            case "2":
+                # Найти запись по вхождению частей имени
+                findUser()
+
+            case "3":
+                # Добавить новый контакт
+                writeFile()
+
+            case "4":
+                # удаление данных
+                deletUser()
+
+            case "5":
+                # удаление данных
+                deletUser()    
+
+            case "6":
+                # выход
+                exit(0)
